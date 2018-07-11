66669206e11240f77d02f1acb5128a951a6211c3
bernardodesousa <bernardodesousa@gmail.com>
2018-07-11 10:18:30 -0300

    Fix bug about Order exercises not being activated sometimes.

--- src/content/clearClassRoomEclassDisplay.js
+++ src/content/clearClassRoomEclassDisplay.js
@@ -2,5 +2,7 @@ import { showNavigation } from './navigation'
 
 export function clearClassRoomEclassDisplay() {
     document.getElementById("classroom-display-eclass").innerHTML = ""
+    Student.Content.selectedEclass.order = []
+    Student.Content.selectedEclass.openQuestions = []
     showNavigation()
 }
