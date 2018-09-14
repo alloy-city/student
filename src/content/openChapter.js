9fa0b6d3fd7505f466ace0ba8809a2cf9e4f61a0
Bernardo SOUSA <bernardodesousa@gmail.com>
2018-09-14 17:41:31 -0300

    Fix bug related to version 2 of no icon lesson image.

--- src/content/openChapter.js
+++ src/content/openChapter.js
@@ -49,7 +49,7 @@ function openChapter(themeIndex, _id, ownsAll) {
                         markup += `
                             <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                                 <div class="thumbnail lesson-card">
-                                    <img src="/images/lesson-icons/${lesson.icon ? lesson.icon : "lesson-no-icon.png"}" alt="">
+                                    <img src="/images/lesson-icons/${lesson.icon ? lesson.icon : "lesson-no-icon_v2.png"}" alt="">
                                     <h4>${lesson.title}</h4>
                                     <p>${lesson.subtitle}</p>
                                     <div>
