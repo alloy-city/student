94c2e31595bbedac961ed8e6ef9b729040a2e0b0
Pedro SOUSA <bernardodesousa@gmail.com>
2019-12-09 17:01:53 -0300

    Fetch search results

--- src/content/search.js
+++ src/content/search.js
@@ -1,7 +1,14 @@
+import * as http from "../http";
 let input = document.getElementById("didactic-material-search");
 
 function search() {
     console.log(input.value);
+    console.log(encodeURIComponent(input.value));
+    http.get(`search/material/${encodeURIComponent(input.value)}`, listResults);
+}
+
+function listResults(results) {
+    console.log(results);
 }
 
 function setSearchListener() {
--- src/http/index.js
+++ src/http/index.js
@@ -1,7 +1,7 @@
 // perform HTTP requests.
 function http(method, body, route, callback) {
     /// #if DEBUG
-    // console.log(`${apiDomain}/api/${route}`, body)
+    console.log(`${apiDomain}/api/${route}`, body)
     /// #endif
 
     let headers = new Headers({
