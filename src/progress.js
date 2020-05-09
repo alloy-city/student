90e4618c5ecc2434490db4f542aef87fbfa07401
Pedro SOUSA <bernardodesousa@gmail.com>
2020-05-09 18:23:40 -0300

    Implement progress bars

---
+++ src/progress.js
@@ -0,0 +1,97 @@
+import { get } from "./http";
+
+const container = document.getElementById("user2");
+container.innerHTML = "";
+
+const levelTextStyle = "color:white;font-size:smaller;font-style:italic;position:absolute;top:2px;right:6px;"
+const ownedMaterialTextStyle = "color:white;font-size:smaller;font-style:italic;position:absolute;top:20px;right:6px;"
+const materialTextStyle = "color:white;font-size:smaller;font-style:italic;position:absolute;top:38px;right:6px;"
+const containerStyle = "overflow:hidden;height:18px;margin-bottom:0;background-color:#bb3444;-webkit-box-shadow:inset 0 1px 2px rgba(0, 0, 0, 0.1);box-shadow: inset 0 1px 2px rgba(0,0,0,0.1);border-radius:0"
+
+const levelProgressContainer = document.createElement("div");
+const ownedMaterialProgressContainer = document.createElement("div");
+const materialProgressContainer = document.createElement("div");
+
+const levelProgressBar = document.createElement("div");
+const ownedMaterialProgressBar = document.createElement("div");
+const materialProgressBar = document.createElement("div");
+
+const levelText = document.createElement("span");
+const ownedMaterialText = document.createElement("span");
+const materialText = document.createElement("span");
+
+levelProgressContainer.appendChild(levelProgressBar)
+ownedMaterialProgressContainer.appendChild(ownedMaterialProgressBar)
+materialProgressContainer.appendChild(materialProgressBar)
+
+levelProgressContainer.appendChild(levelText)
+ownedMaterialProgressContainer.appendChild(ownedMaterialText)
+materialProgressContainer.appendChild(materialText)
+
+levelProgressContainer.classList.add("progress");
+ownedMaterialProgressContainer.classList.add("progress");
+materialProgressContainer.classList.add("progress");
+
+levelProgressContainer.setAttribute("style", containerStyle);
+ownedMaterialProgressContainer.setAttribute("style", containerStyle);
+materialProgressContainer.setAttribute("style", containerStyle);
+
+levelProgressBar.classList.add("progress-bar", "progress-bar-primary");
+ownedMaterialProgressBar.classList.add("progress-bar", "progress-bar-info");
+materialProgressBar.classList.add("progress-bar", "progress-bar-success");
+
+levelProgressBar.setAttribute("role", "progressbar");
+ownedMaterialProgressBar.setAttribute("role", "progressbar");
+materialProgressBar.setAttribute("role", "progressbar");
+
+levelProgressBar.setAttribute("aria-valuemin", "0");
+ownedMaterialProgressBar.setAttribute("aria-valuemin", "0");
+materialProgressBar.setAttribute("aria-valuemin", "0");
+
+levelProgressBar.setAttribute("aria-valuemax", "1");
+ownedMaterialProgressBar.setAttribute("aria-valuemax", "1");
+materialProgressBar.setAttribute("aria-valuemax", "1");
+
+levelProgressBar.setAttribute("aria-valuenow", "0");
+ownedMaterialProgressBar.setAttribute("aria-valuenow", "0");
+materialProgressBar.setAttribute("aria-valuenow", "0");
+
+levelProgressBar.setAttribute("style", "width: 0%");
+ownedMaterialProgressBar.setAttribute("style", "width: 0%");
+materialProgressBar.setAttribute("style", "width: 0%");
+
+levelText.setAttribute("style", levelTextStyle);
+ownedMaterialText.setAttribute("style", ownedMaterialTextStyle);
+materialText.setAttribute("style", materialTextStyle);
+
+container.appendChild(levelProgressContainer);
+container.appendChild(ownedMaterialProgressContainer);
+container.appendChild(materialProgressContainer);
+
+function displayUserProgress() {
+    get("xp/progression", progress => {
+        progress.overLevel = Auth.userData.progressionOverLevel;
+
+        console.log(progress);
+
+        levelProgressBar.setAttribute("aria-valuenow", `"${progress.overLevel}"`);
+        ownedMaterialProgressBar.setAttribute("aria-valuenow", `"${progress.overOwned}"`);
+        materialProgressBar.setAttribute("aria-valuenow", `"${progress.overTotal}"`);
+
+        levelProgressBar.setAttribute("style", `width: ${progress.overLevel*100}%;position:absolute;height:33%;`);
+        ownedMaterialProgressBar.setAttribute("style", `width: ${progress.overOwned*100}%;position:absolute;height:33%;`);
+        materialProgressBar.setAttribute("style", `width: ${progress.overTotal*100}%;position:absolute;height:33%;`);
+
+        levelText.innerText = `${Math.round(progress.overLevel*100)}${string.userPanelText.progressBars.level.title}`
+        ownedMaterialText.innerText = `${Math.round(progress.overOwned*100)}${string.userPanelText.progressBars.ownedMaterial.title}`
+        materialText.innerText = `${Math.round(progress.overTotal*100)}${string.userPanelText.progressBars.material.title}`
+
+        levelProgressContainer.setAttribute("title", `${string.userPanelText.progressBars.level.text[0]}${Auth.userData.xp}${string.userPanelText.progressBars.level.text[1]}${Auth.userData.still}${string.userPanelText.progressBars.level.text[2]}${string.material.levels[Auth.userData.level + 1]}`);
+        ownedMaterialProgressContainer.setAttribute("title", `${string.userPanelText.progressBars.ownedMaterial.text[0]}${Math.round(progress.overOwned*100)}${string.userPanelText.progressBars.ownedMaterial.text[1]}`);
+        materialProgressContainer.setAttribute("title", `${string.userPanelText.progressBars.material.text[0]}${(progress.overTotal*100).toFixed(2)}${string.userPanelText.progressBars.material.text[1]}`);
+
+        console.log(string.userPanelText.progressBars);
+    })
+}
+
+export { displayUserProgress }
--- src/userDisplay.js
+++ src/userDisplay.js
@@ -1,91 +1,94 @@
 import * as Animations from "./animations.js"
+import { displayUserProgress } from "./progress";
 
-function userDisplay(){
+function userDisplay() {
+    if (!Auth.userData.xp) {
+        Auth.userData.xp = 0
+    }
 
-	if (!Auth.userData.xp) {
-		Auth.userData.xp = 0
-	}
+    var displayUserName = '';
+    if (Auth.userData.nickname && Auth.userData.nickname.length > 0 && Auth.userData.nickname != "undefined") {
+        displayUserName = Auth.userData.nickname;
+    }
+    else if (Auth.userData.name && Auth.userData.name.length > 0 && Auth.userData.name != "undefined") {
+        displayUserName = Auth.userData.name;
+    }
+    else {
+        displayUserName = Auth.userData.mainEmail;
+    }
+    document.getElementById('displayUserName').innerHTML = displayUserName;
+    document.getElementById('smallScreenDisplayUserName').innerHTML = displayUserName;
 
-	var displayUserName = '';
-	if (Auth.userData.nickname && Auth.userData.nickname.length > 0 && Auth.userData.nickname != "undefined"){
-		displayUserName = Auth.userData.nickname;
-	}
-	else if (Auth.userData.name && Auth.userData.name.length > 0 && Auth.userData.name != "undefined"){
-		displayUserName = Auth.userData.name;
-	}
-	else {
-		displayUserName = Auth.userData.mainEmail;
-	}
-	document.getElementById('displayUserName').innerHTML = displayUserName;
-	document.getElementById('smallScreenDisplayUserName').innerHTML = displayUserName;
+    calculateLevel();
 
-	calculateLevel();
-
-	userImg();
+    userImg();
 }
 
 function updateXp(n) {
-	Auth.userData.xp += n
-	calculateLevel()
+    Auth.userData.xp += n
+    calculateLevel()
 }
 
 function userImg() {
-	if (!Auth.userData.picture || Auth.userData.picture == "") {
-		document.getElementById("userImg").src = '/images/noPicture.png';
-	} else {
-		document.getElementById("userImg").src = Auth.userData.picture;
-	}
+    if (!Auth.userData.picture || Auth.userData.picture == "") {
+        document.getElementById("userImg").src = '/images/noPicture.png';
+    } else {
+        document.getElementById("userImg").src = Auth.userData.picture;
+    }
 }
 
 function calculateLevel() {
-	if (Auth.userData.xp >= 60000) {
-		// level = 8; //Jedi Master
-		Auth.userData.level = 8;
-	} else if (Auth.userData.xp >= 45000) {
-		// level = 7; //Legend
-		Auth.userData.level = 7;
-		Auth.userData.still = (60000 - Auth.userData.xp);
-	} else if (Auth.userData.xp >= 32000) {
-		// level = 6; //Ninja
-		Auth.userData.level = 6;
-		Auth.userData.still = (45000 - Auth.userData.xp);
-	} else if (Auth.userData.xp >= 22000) {
-		// level = 5; //Monster
-		Auth.userData.level = 5;
-		Auth.userData.still = (32000 - Auth.userData.xp);
-	} else if (Auth.userData.xp >= 13000) {
-		// level = 4; //Insane
-		Auth.userData.level = 4;
-		Auth.userData.still = (22000 - Auth.userData.xp);
-	} else if (Auth.userData.xp >= 8000) {
-		// level = 3; //Explorer
-		Auth.userData.level = 3;
-		Auth.userData.still = (13000 - Auth.userData.xp);
-	} else if (Auth.userData.xp >= 4000) {
-		// level = 2; //Nerd
-		Auth.userData.level = 2;
-		Auth.userData.still = (8000 - Auth.userData.xp);
-	} else if (Auth.userData.xp >= 1000) {
-		// level = 1; //Apprentice
-		Auth.userData.level = 1;
-		Auth.userData.still = (4000 - Auth.userData.xp);
-	} else {
-		// level = 0; //Noob
-		Auth.userData.level = 0;
-		Auth.userData.still = (1000 - Auth.userData.xp);
-	}
-	getLoggedInText();
+    if (Auth.userData.xp >= 60000) {
+        Auth.userData.level = 8; // Jedi Master
+        Auth.userData.progressionOverLevel = 1;
+    } else if (Auth.userData.xp >= 45000) {
+        Auth.userData.level = 7; // Legend
+        Auth.userData.still = (60000 - Auth.userData.xp);
+        Auth.userData.progressionOverLevel = Auth.userData.xp / 60000;
+    } else if (Auth.userData.xp >= 32000) {
+        Auth.userData.level = 6; // Ninja
+        Auth.userData.still = (45000 - Auth.userData.xp);
+        Auth.userData.progressionOverLevel = Auth.userData.xp / 45000;
+    } else if (Auth.userData.xp >= 22000) {
+        Auth.userData.level = 5; // Monster
+        Auth.userData.still = (32000 - Auth.userData.xp);
+        Auth.userData.progressionOverLevel = Auth.userData.xp / 32000;
+    } else if (Auth.userData.xp >= 13000) {
+        Auth.userData.level = 4; // Insane
+        Auth.userData.still = (22000 - Auth.userData.xp);
+        Auth.userData.progressionOverLevel = Auth.userData.xp / 22000;
+    } else if (Auth.userData.xp >= 8000) {
+        Auth.userData.level = 3; // Explorer
+        Auth.userData.still = (13000 - Auth.userData.xp);
+        Auth.userData.progressionOverLevel = Auth.userData.xp / 13000;
+    } else if (Auth.userData.xp >= 4000) {
+        Auth.userData.level = 2; // Nerd
+        Auth.userData.still = (8000 - Auth.userData.xp);
+        Auth.userData.progressionOverLevel = Auth.userData.xp / 8000;
+    } else if (Auth.userData.xp >= 1000) {
+        Auth.userData.level = 1; // Apprentice
+        Auth.userData.still = (4000 - Auth.userData.xp);
+        Auth.userData.progressionOverLevel = Auth.userData.xp / 4000;
+    } else {
+        Auth.userData.level = 0; // Noob
+        Auth.userData.still = (1000 - Auth.userData.xp);
+        Auth.userData.progressionOverLevel = Auth.userData.xp / 1000;
+    }
+
+    getLoggedInText();
 }
 
 function getLoggedInText() {
-	$('#displayUserLevel').text(string.material.levels[Auth.userData.level]);
-	$('#displayUserXP').text(string.userPanelText.youHave + Auth.userData.xp + string.userPanelText.xps);
+    $('#displayUserLevel').text(string.material.levels[Auth.userData.level]);
+    // $('#displayUserXP').text(string.userPanelText.youHave + Auth.userData.xp + string.userPanelText.xps);
 
-	Animations.animateElement($('#displayUserXP').parent(), 'rubberBand');
+    // Animations.animateElement($('#displayUserXP').parent(), 'rubberBand');
 
-	if (Auth.userData.level != 8) {
-		document.getElementById('displayUserNextLevel').innerHTML = string.userPanelText.still + Auth.userData.still + string.userPanelText.toGetTo + string.material.levels[Auth.userData.level + 1];
-	}
+    // if (Auth.userData.level != 8) {
+        // document.getElementById('displayUserNextLevel').innerHTML = string.userPanelText.still + Auth.userData.still + string.userPanelText.toGetTo + string.material.levels[Auth.userData.level + 1];
+    // }
+
+    displayUserProgress();
 }
 
 export { userDisplay, updateXp }
