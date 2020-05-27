59e4a32890db4efdb0e09cded8892cdd1944ee27
Pedro SOUSA <bernardodesousa@gmail.com>
2020-05-26 22:21:24 -0300

    Hot fix

--- src/active-resource/build-gaps-resource.js
+++ src/active-resource/build-gaps-resource.js
@@ -1,16 +1,16 @@
 function buildGapsResource(resource) {
-    Student.ActiveResource.gapsAnswer[resource.resource._id] = []
+    Student.ActiveResource.gapsAnswer[resource._id] = []
 
-    let body = `<form class="form-inline"><div class="form-group" id="${resource.resource._id}">`
+    let body = `<form class="form-inline"><div class="form-group" id="${resource._id}">`
     for (var i = 0; i < resource.resource.items.length; i++) {
         if (isEven(i)) {
             body += resource.resource.items[i]
         } else {
             if (resource.solved) {
-                Student.ActiveResource.gapsAnswer[resource.resource._id].push({ answer: resource.resource.items[i], userAnswer: resource.resource.items[i] })
+                Student.ActiveResource.gapsAnswer[resource._id].push({ answer: resource.resource.items[i], userAnswer: resource.resource.items[i] })
                 body += `<input type="text" class="form-control" value="${resource.resource.items[i]}">`
             } else {
-                Student.ActiveResource.gapsAnswer[resource.resource._id].push({ answer: resource.resource.items[i], userAnswer: '' })
+                Student.ActiveResource.gapsAnswer[resource._id].push({ answer: resource.resource.items[i], userAnswer: '' })
                 body += '<input type="text" class="form-control">'
             }
         }
@@ -24,7 +24,7 @@ function buildGapsResource(resource) {
             </div>
             <div class="panel-body">
                 ${body}
-                <button class="btn btn-warning" onclick="Student.ActiveResource.checkGapFills('${resource.resource._id}')">${string.buttons.verify}</button>
+                <button class="btn btn-warning" onclick="Student.ActiveResource.checkGapFills('${resource._id}')">${string.buttons.verify}</button>
             </div>
         </div>`
 
